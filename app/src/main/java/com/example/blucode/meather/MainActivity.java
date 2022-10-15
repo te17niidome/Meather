62a81b7b341828c384686ff37183664e1c3f4856
te17niidome <ae22niidome@g.kumamoto-nct.ac.jp>
2022-10-15 17:31:59 +0900

    OkHttpのインスタンス作成記述

--- app/src/main/java/com/example/blucode/meather/MainActivity.java
+++ app/src/main/java/com/example/blucode/meather/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.blucode.meather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private OkHttpClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mClient = new OkHttpClient();
     }
 }
