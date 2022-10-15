ef58e1e1a12eea41a4d2f291c8491b4ee6d3e837
te17niidome <ae22niidome@g.kumamoto-nct.ac.jp>
2022-10-15 19:17:25 +0900

    リクエストの作成 リクエストを非同期 などの追加

--- app/src/main/java/com/example/blucode/meather/MainActivity.java
+++ app/src/main/java/com/example/blucode/meather/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.blucode.meather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +29,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mClient = new OkHttpClient();
+    }
+
+    private void loadweather(){
+        // 接続先
+        String url = "https://api.openweathermap.org/data/2.5/weather";
+
+        //リクエストを作成
+        Request request = new Request.Builder().url(url).build();
+        Call call = mClient.newCall(request);
+
+        // リクエストを非同期実行
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(MainActivity.this, "処理失敗", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String body = response.body().string();
+
+                JSONObject json = null;
+                try {
+                    json = new JSONObject(body);
+                    // *******************************************************
+                    JSONArray colorArray = json.getJSONArray("colorsArray");
+                    // *******************************************************
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
